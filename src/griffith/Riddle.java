38cd6c189b19372de5fc21f6569d689472f29f3a
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-03-13 17:00:33 +0200

    Add class variables.

--- src/griffith/Riddle.java
+++ src/griffith/Riddle.java
@@ -1,5 +1,9 @@
 package griffith;
 
 public abstract class Riddle {
-
+    // Class variables.
+    private String question;
+    private String answer;
+    private String hint;
+    private int countAttempts;
 }
