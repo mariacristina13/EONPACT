f4b1119919b76ede05feaf8fccc43b1b7fea0e51
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-03-14 12:58:27 +0200

    Add failing test implementation for the checkAnswer method.

--- Tests/griffith/TestGameManager.java
+++ Tests/griffith/TestGameManager.java
@@ -1,5 +1,27 @@
 package griffith;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class TestGameManager {
+
+    // Riddle Tests
+    @Test
+    public void testCheckAnswer(){
+        Riddle riddle = new Riddle("I can smell food from a mile away.", "Bear", "Inwinter I go into a deep sleep called hibernation.");
+
+        // Actual values generated when we run the checkAnswer method.
+        boolean actual = riddle.checkAnswer("Bear");
+        boolean actual1 = riddle.checkAnswer("bear");
+        boolean actual2 = riddle.checkAnswer("");
+        boolean actual3 = riddle.checkAnswer(null);
+
+        assertTrue(actual);
+        assertTrue(actual1);
+        assertFalse(actual2);
+        assertFalse(actual3);
+    }
 
 }
