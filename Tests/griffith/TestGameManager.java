dc1ded7a8a02ecad8bef56c73ca4fa5f3d56f49c
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-03-14 13:50:37 +0200

    Update test for the checkAnswer method to check for NullPointerException.

--- Tests/griffith/TestGameManager.java
+++ Tests/griffith/TestGameManager.java
@@ -1,7 +1,9 @@
 package griffith;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
+
 
 import org.junit.Test;
 
@@ -13,15 +15,24 @@ public class TestGameManager {
         Riddle riddle = new Riddle("I can smell food from a mile away.", "Bear", "Inwinter I go into a deep sleep called hibernation.");
 
         // Actual values generated when we run the checkAnswer method.
-        boolean actual = riddle.checkAnswer("Bear");
+        boolean actual = riddle.checkAnswer(" Bear ");
         boolean actual1 = riddle.checkAnswer("bear");
         boolean actual2 = riddle.checkAnswer("");
-        boolean actual3 = riddle.checkAnswer(null);
 
         assertTrue(actual);
         assertTrue(actual1);
         assertFalse(actual2);
-        assertFalse(actual3);
+
+        Exception exception = assertThrows(NullPointerException.class, () -> {
+            riddle.checkAnswer(null);
+        });
+         String expectedMessage = "null";
+       
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
     }
+
+    
 
 }
