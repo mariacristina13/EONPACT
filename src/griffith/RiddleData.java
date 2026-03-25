aa7bd22d1ed80b1e3210dc9bcae928fbf24a371a
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-03-25 22:20:32 +0000

    Add constructor to initialise the riddles.

--- src/griffith/RiddleData.java
+++ src/griffith/RiddleData.java
@@ -1,11 +1,15 @@
 package griffith;
 
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 public class RiddleData {
     // Linked Hash Map to store the riddles and the information about them (answer and hint).
     private LinkedHashMap<String, RiddleInfo> riddleData = new LinkedHashMap<String, RiddleInfo>();
+
+    // Initialise the Linked Hash Map and add the riddles to it.
+    public RiddleData() {
+        riddleData = new LinkedHashMap<String, RiddleInfo>();
+    }
 
 }
