5754a664935c9f78f8cc8e951fb96c8beaf39b3e
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-03-25 22:12:44 +0000

    Add the constructor to initialise the riddle information.

--- src/griffith/RiddleInfo.java
+++ src/griffith/RiddleInfo.java
@@ -4,4 +4,11 @@ public class RiddleInfo {
     // Class variables that represent the riddle information.
     private String answer;
     private String hint;
+
+     // Initialise the class variables.
+    public RiddleInfo(String answer, String hint) {
+        this.answer = answer;
+        this.hint = hint;
+    }
+
 }
