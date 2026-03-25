4967863b365acab1af737e874fb023535fb97f59
Svetlana Lazareva <lazarsveta2705@gmail.com>
2026-03-25 15:49:52 +0000

    Modify paint method

--- src/griffith/MyPannel.java
+++ src/griffith/MyPannel.java
@@ -1,6 +1,7 @@
 package griffith;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
@@ -12,5 +13,8 @@ public void paint(Graphics g){
 super.paint(g);
 Graphics2D graphics = (Graphics2D) g;
 BufferedImage image;
+//improve rendering quality
+RenderingHints hints = new RenderingHints( RenderingHints.KEY_ANTIALIASING,   RenderingHints.VALUE_ANTIALIAS_ON);
+graphics.setRenderingHints(hints);
 }
 }
