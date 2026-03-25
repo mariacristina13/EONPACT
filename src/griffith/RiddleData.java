481b844ea89e3f97416ab7e043c9ecd8c19829e9
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-03-25 22:17:54 +0000

    Add Linked Hash Map to store the riddless, answers and hints.

--- src/griffith/RiddleData.java
+++ src/griffith/RiddleData.java
@@ -1,5 +1,11 @@
 package griffith;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class RiddleData {
-    
+    // Linked Hash Map to store the riddles and the information about them (answer and hint).
+    private LinkedHashMap<String, RiddleInfo> riddleData = new LinkedHashMap<String, RiddleInfo>();
+
 }
