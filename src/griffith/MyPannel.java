a91510bcc1de4caa1de3207819c63cf66dcde548
Svetlana Lazareva <lazarsveta2705@gmail.com>
2026-03-25 15:46:33 +0000

    Add paint method for adding images

--- src/griffith/MyPannel.java
+++ src/griffith/MyPannel.java
@@ -1,5 +1,16 @@
 package griffith;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
-public class MyPannel {
+import javax.swing.JPanel;
 
+public class MyPannel extends JPanel{
+
+@Override
+public void paint(Graphics g){
+super.paint(g);
+Graphics2D graphics = (Graphics2D) g;
+BufferedImage image;
+}
 }
