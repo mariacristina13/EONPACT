dedb6c6da817197ee9639f2a883b6fe04e97dafe
Svetlana Lazareva <lazarsveta2705@gmail.com>
2026-03-25 15:57:15 +0000

    Implement KeyListener

--- src/griffith/MyPannel.java
+++ src/griffith/MyPannel.java
@@ -3,10 +3,14 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyListener;
+
 
 import javax.swing.JPanel;
 
-public class MyPannel extends JPanel{
+public class MyPannel extends JPanel implements KeyListener{
 
 @Override
 public void paint(Graphics g){
@@ -15,6 +19,24 @@ Graphics2D graphics = (Graphics2D) g;
 BufferedImage image;
 //improve rendering quality
 RenderingHints hints = new RenderingHints( RenderingHints.KEY_ANTIALIASING,   RenderingHints.VALUE_ANTIALIAS_ON);
-graphics.setRenderingHints(hints);
+graphics.setRenderingHints(hints); // https://docs.oracle.com/javase/8/docs/api/javax/swing/package-summary.html
+}
+
+@Override
+public void keyTyped(KeyEvent e) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+}
+
+@Override
+public void keyPressed(KeyEvent e) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+}
+
+@Override
+public void keyReleased(KeyEvent e) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
 }
 }
