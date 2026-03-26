c0eca47a1bb6eb9a1712550b09498d51d93a03ed
Svetlana Lazareva <lazarsveta2705@gmail.com>
2026-03-26 00:20:22 +0000

    Create a Frame and add panel

--- src/griffith/Init.java
+++ src/griffith/Init.java
@@ -1,5 +1,14 @@
 package griffith;
 
+import javax.swing.JFrame;
+
 public class Init {
 
+    public static void main (String[] args){
+        JFrame frame = new JFrame();
+        MyPannel panel = new MyPannel();
+        frame.add(panel);
+    }
+
+    
 }
