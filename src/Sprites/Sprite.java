96e34fa4e0bc866ee7d173ee61a5e62e29b2c399
Svetlana Lazareva <lazarsveta2705@gmail.com>
2026-03-27 12:34:01 +0000

    Add getters & setters to Sprite class

--- src/Sprites/Sprite.java
+++ src/Sprites/Sprite.java
@@ -26,6 +26,53 @@ public abstract class Sprite {
 			System.out.print("file not found");
 		}
 	}
+	public BufferedImage getImage() {
+		return image;
+	}
 
+	
+	public void setImage(BufferedImage image) {
+		this.image = image;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
 
 }
