f41c83b356c1115bb77422ea3b315c4426f313c4
Svetlana Lazareva <lazarsveta2705@gmail.com>
2026-03-27 12:30:19 +0000

    Add Sprite class variables

--- src/Sprites/Sprite.java
+++ src/Sprites/Sprite.java
@@ -1,5 +1,15 @@
 package Sprites;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public abstract class Sprite {
-    
+    private String fileName;
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	private BufferedImage image;
 }
