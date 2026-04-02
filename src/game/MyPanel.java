37ca8f7f648b7d1f6e05ff62edbf90dad642e8f0
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-02 16:09:39 +0100

    Draw the timer on the game screen.

--- src/game/MyPanel.java
+++ src/game/MyPanel.java
@@ -18,16 +18,37 @@ public class MyPanel extends JPanel implements KeyListener {
         game = new GameManager();
     }
 
+    // https://projectai.in/projects/e79f02df-4d51-473e-90f0-4ff8443ff473/tasks/5b55ecc1-ac91-4092-8e63-097ce794218b?tab=task
     @Override
-    public void paint(Graphics g) {
-        super.paint(g);
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
         Graphics2D graphics = (Graphics2D) g;
         // improve rendering quality
         RenderingHints hints = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         graphics.setRenderingHints(hints); // https://docs.oracle.com/javase/8/docs/api/javax/swing/package-summary.html
+        
         drawBG(graphics);
+
         game.drawSprites(graphics, this);
         game.drawRiddle(graphics, getWidth(), getHeight());
+
+        drawTimer(graphics);
+    }
+
+    // Draw the timer.
+    private void drawTimer(Graphics2D graphics){
+        String timeText = "Time remaining: " + game.getTimer();
+
+        graphics.setFont(Constants.TIMER_FONT);
+
+        
+        int textWidth = graphics.getFontMetrics().stringWidth(timeText);
+        int padding = 10;
+        int x = getWidth() - textWidth - padding * 2 - 30;
+        int y = 30;
+
+        graphics.setColor(Constants.GREEN);
+        graphics.drawString(timeText, x, y);
     }
 
     @Override
