83fcd8ed415de1a3f1c417b92941909b30363346
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-05 13:19:13 +0100

    Add class variables.

--- src/game/MenuButton.java
+++ src/game/MenuButton.java
@@ -6,10 +6,13 @@ import constants.Constants;
 import ui.Button;
 
 public class MenuButton extends Button {
+     // Class variable.
+    private String text;
+
     public MenuButton(int x, int y, int width, int height) {
         super(x, y, width, height);
     }
-    
+
     @Override
     public void drawButton(Graphics2D g) {
     }
