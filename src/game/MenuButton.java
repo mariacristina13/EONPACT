b76e0acc5876cd30e34015d269a9645cdec7dbf7
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-05 13:24:21 +0100

    Implement the drawButton method.

--- src/game/MenuButton.java
+++ src/game/MenuButton.java
@@ -6,16 +6,34 @@ import constants.Constants;
 import ui.Button;
 
 public class MenuButton extends Button {
-     // Class variable.
+    // Class variable.
     private String text;
 
-    public MenuButton(int x, int y, int width, int height) {
+    public MenuButton(String text, int x, int y, int width, int height) {
         super(x, y, width, height);
         this.text = text;
     }
 
+    // Draw the characters/quit buttons.
     @Override
     public void drawButton(Graphics2D g) {
+         if(isHovered()){
+            g.setColor(Constants.MAROON);
+        }
+        else{
+            g.setColor(Constants.BROWN_RED);
+        }
+
+        g.fillRect(getX(), getY(), getWidth(), getHeight());
+        
+        g.setColor(Constants.WHITE);
+        g.setFont(Constants.BUTTON_FONT);
+    
+        int textWidth = g.getFontMetrics().stringWidth(text);
+        int textHeight = g.getFontMetrics().getAscent();
+
+        g.drawString(text, getX() +(getWidth() - textWidth)/2, getY() +(getHeight() + textHeight)/2);
+
     }
 
 }
