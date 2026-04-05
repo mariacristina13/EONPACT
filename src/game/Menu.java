e3bf69038a7f6aec3de6d6a220289d9f7558549d
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-05 13:51:43 +0100

    Add class variables.

--- src/game/Menu.java
+++ src/game/Menu.java
@@ -1,5 +1,14 @@
 package game;
 
+import java.awt.Graphics2D;
+
+import java.awt.event.MouseEvent;
+
+import constants.Constants;
+
 public class Menu {
+    // Class variables.
+    private MenuButton characterBtn;
+    private MenuButton quitBtn;
 
 }
