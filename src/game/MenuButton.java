146838054f9faf8b0440bfe54c01491a1105c478
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-05 13:18:29 +0100

    Add unimplemented methods and constructor to initialise the variables from the parent class.

---
+++ src/game/MenuButton.java
@@ -0,0 +1,17 @@
+package game;
+
+import java.awt.Graphics2D;
+
+import constants.Constants;
+import ui.Button;
+
+public class MenuButton extends Button {
+    public MenuButton(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+    
+    @Override
+    public void drawButton(Graphics2D g) {
+    }
+
+}
