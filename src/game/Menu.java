b0cfa38978a919ad4d7a493dd66bae19daad88ce
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-05 13:54:30 +0100

    Initialise the class variables.

--- src/game/Menu.java
+++ src/game/Menu.java
@@ -11,4 +11,20 @@ public class Menu {
     private MenuButton characterBtn;
     private MenuButton quitBtn;
 
+    public Menu(){
+        initButtons();
+    }
+
+    // Initialise buttons
+    private void initButtons() {
+        // Set the width of the row that the buttons are going to be displayed.
+        int totalWidth = (Constants.BUTTON_WIDTH * 2) + Constants.BUTTON_SPACEING;
+        // Set the starting X position for the character display.
+        int startX = Constants.BUTTON_X - (totalWidth / 2);
+
+        // Initialise buttons.
+        characterBtn = new MenuButton("Characters", startX, Constants.BUTTON_Y, Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
+        quitBtn = new MenuButton("Quit", startX + Constants.BUTTON_WIDTH + Constants.BUTTON_SPACEING, Constants.BUTTON_Y, Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
+    }
+
 }
