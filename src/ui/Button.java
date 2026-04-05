667b3d2a8ec3fe193ab2d8a6c2b205ab9873a336
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-05 13:01:35 +0100

    Add class variables.

---
+++ src/ui/Button.java
@@ -0,0 +1,12 @@
+package ui;
+
+import java.awt.Graphics2D;
+
+public abstract class Button {
+    // Class variables.
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private boolean hovered = false;
+}
