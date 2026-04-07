f35017585f07cef5bf6b5f2f3c33a6e04dd851cc
Svetlana Lazareva <lazarsveta2705@gmail.com>
2026-04-07 23:14:47 +0100

    Add new Map class

---
+++ src/Sprites/Map.java
@@ -0,0 +1,9 @@
+package Sprites;
+
+public class Map extends Sprite{
+
+    public Map(String fileName, int x, int y, int width, int height) {
+        super(fileName, x, y, width, height);
+    }
+    
+}
