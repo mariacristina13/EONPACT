b24cfee2e2ac74c4b3fb1ed4c0b893bfbfb75e1f
Svetlana Lazareva <lazarsveta2705@gmail.com>
2026-04-08 14:27:41 +0100

    Add isCollected method to Food

--- src/Sprites/Food.java
+++ src/Sprites/Food.java
@@ -2,8 +2,14 @@ package Sprites;
 
 public class Food extends Sprite{
 
+    private boolean collected = false;
+
     public Food(String fileName, int x, int y, int width, int height) {
         super(fileName, x, y, width, height);
+    }
+
+    public boolean isCollected(){
+        return collected;
     }
 
 }
