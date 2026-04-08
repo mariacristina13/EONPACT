2936e228add2b5efb3d413bedb66a13840d9f6b7
Svetlana Lazareva <lazarsveta2705@gmail.com>
2026-04-08 14:23:39 +0100

    Add Food comstructor

--- src/Sprites/Food.java
+++ src/Sprites/Food.java
@@ -1,5 +1,9 @@
 package Sprites;
 
-public class Food {
+public class Food extends Sprite{
+
+    public Food(String fileName, int x, int y, int width, int height) {
+        super(fileName, x, y, width, height);
+    }
 
 }
