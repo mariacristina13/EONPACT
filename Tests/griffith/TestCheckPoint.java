c1bc0f74751eeecdffa849fe90d6d2684b75ad2d
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-10 17:35:10 +0100

    Add test to check that checkpoints get random riddles.

--- Tests/griffith/TestCheckPoint.java
+++ Tests/griffith/TestCheckPoint.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.assertEquals;
 
 import Sprites.CheckPoint;
 import riddles.Riddle;
+import riddles.RiddleData;
 
 class TestCheckPoint {
 
@@ -116,4 +117,45 @@ class TestCheckPoint {
         assertTrue(cp.isFailed());
     }
  
+    @Test
+    public void testCheckpointGetsRandomRiddle() {
+        // Initialise the riddle data object.
+        RiddleData data = new RiddleData();
+
+        // Initialise a list of checkpoints.
+        ArrayList<CheckPoint> checkpoints = new ArrayList<CheckPoint>();
+
+        // Initialise a list of riddle answers.
+        ArrayList<String> assignedAnswers = new ArrayList<String>();
+
+        // Create checkpoints and assign random riddles.
+        int numCheckpoints = 5;
+        for (int i = 0; i < numCheckpoints; i++) {
+            // Initialise each checkpoint at diffrent positions.
+            CheckPoint checkPoint = new CheckPoint("test.png", i * 50, 0, 10, 10);
+
+            // Get a random riddle from the riddle data.
+            Riddle randomRiddle = data.getRandomRiddle();
+
+            // Add the random riddle to the checkpoint.
+            checkPoint.setRiddle(randomRiddle);
+
+            // Add the chekpoint into the checkpoints list.
+            checkpoints.add(checkPoint);
+
+            // Add the answer to the riddle into the list of answers.
+            assignedAnswers.add(randomRiddle.getAnswer());
+        }
+
+        // Check that some of the asnwers are unique and add them to a list of unique answers.
+        ArrayList<String> uniqueAnswers = new ArrayList<>();
+        for (String answer : assignedAnswers) {
+            if (!uniqueAnswers.contains(answer)) {
+                uniqueAnswers.add(answer);
+            }
+        }
+
+        // Check if there are more than two answers that are diffrent.
+        assertTrue(uniqueAnswers.size() > 2);
+    }
 }
