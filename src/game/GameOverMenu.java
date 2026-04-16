e5dd0f5b968a6bd96f7fafece22fd49983cc3af9
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-16 13:00:13 +0100

    Add constructor to initialise buttons.

--- src/game/GameOverMenu.java
+++ src/game/GameOverMenu.java
@@ -1,7 +1,27 @@
 package game;
 
+import constants.Constants;
+
 public class GameOverMenu {
     // Class variables.
-    private MenuButton menubtn;
-    private MenuButton quitbtn;
+    private MenuButton menuBtn;
+    private MenuButton quitBtn;
+
+    public GameOverMenu() {
+        initButtons();
+    }
+
+    // Initialise the buttons.
+    private void initButtons() {
+        // Set the width of the row that the buttons are going to be displayed.
+        int totalWidth = (Constants.BUTTON_WIDTH * 2) + Constants.BUTTON_SPACEING;
+        // Set the starting X position for the character display.
+        int startX = Constants.BUTTON_X - (totalWidth / 2);
+
+        menuBtn = new MenuButton("Menu", "menu button.png", "menu hover button.png", startX, Constants.BUTTON_Y,
+                Constants.BUTTON_WIDTH, Constants.BUTTON_X);
+        quitBtn = new MenuButton("Quit", "quit button.png", "quit button hover.png",
+                startX + Constants.BUTTON_WIDTH + Constants.BUTTON_SPACEING, Constants.BUTTON_Y, Constants.BUTTON_WIDTH,
+                Constants.BUTTON_HEIGHT);
+    }
 }
