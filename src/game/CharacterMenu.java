474baf83f3b16409171a37b5fbd8be90ce155a64
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-18 12:50:31 +0100

    Update the name of the fox button.

--- src/game/CharacterMenu.java
+++ src/game/CharacterMenu.java
@@ -89,7 +89,7 @@ public class CharacterMenu {
         pandaBtn.setLocked(true);
         characters.add(pandaBtn);
 
-        foxBtn = new CharacterButton("Actic Fox", "arctic fox.png", startX + (Constants.CHARACTER_WIDTH + Constants.CHARACTER_SPACEING) * 6, Constants.CHARACTER_BUTTON_Y,
+        foxBtn = new CharacterButton("Arctic Fox", "arctic fox.png", startX + (Constants.CHARACTER_WIDTH + Constants.CHARACTER_SPACEING) * 6, Constants.CHARACTER_BUTTON_Y,
                 Constants.CHARACTER_WIDTH, Constants.CHARACTER_HEIGHT);
 
         foxBtn.setLocked(true);
