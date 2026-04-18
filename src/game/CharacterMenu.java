149bc0cc63e69729ed980c1431739eb6e1d1b36c
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-18 12:23:21 +0100

    Update the height of the lock image.

--- src/game/CharacterMenu.java
+++ src/game/CharacterMenu.java
@@ -147,7 +147,7 @@ public class CharacterMenu {
                 g.setColor(Constants.TRANS_BLACK);
                 g.fillRect(character.getX(), character.getY(), character.getWidth(), character.getHeight());
 
-               g.drawImage(lockImg, lockX, lockY, Constants.LOCK_WIDTH, character.getHeight(), null);
+               g.drawImage(lockImg, lockX, lockY, Constants.LOCK_WIDTH, Constants.LOCK_HEIGHT, null);
             }
         }
 
