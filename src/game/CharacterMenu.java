3b61a3f7fc1983af8daeb80f5cceb7bab5c0d7d4
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-22 21:13:41 +0100

    Change the y position of the play button.

--- src/game/CharacterMenu.java
+++ src/game/CharacterMenu.java
@@ -121,7 +121,7 @@ public class CharacterMenu {
         int startX = Constants.BUTTON_X - (totalWidth / 2);
 
         // Initialise the buttons.
-        playBtn = new MenuButton("Play", "play button.png", "play button hover.png", startX, Constants.BUTTON_Y,
+        playBtn = new MenuButton("Play", "play button.png", "play button hover.png", startX, Constants.BUTTON_Y + 70,
                 Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
         backBtn = new MenuButton("Back", "back button.png", "back button hover.png",
                 startX + Constants.BUTTON_WIDTH + Constants.BUTTON_SPACEING, Constants.BUTTON_Y, Constants.BUTTON_WIDTH,
