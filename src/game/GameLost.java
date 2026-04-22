2349babeeacb842047f23e42184ab255172a831c
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-22 18:04:27 +0100

    Add class variables.

--- src/game/GameLost.java
+++ src/game/GameLost.java
@@ -5,5 +5,8 @@ import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 
 public class GameLost {
-    
+    // Class variables.
+    private MenuButton menuBtn;
+    private MenuButton quitBtn;
+
 }
