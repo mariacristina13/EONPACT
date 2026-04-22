2b935a04dd47ef031cb3a0dc20729b81e9f1a8fe
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-22 18:08:28 +0100

    Add variable for the background image.

--- src/game/GameLost.java
+++ src/game/GameLost.java
@@ -4,10 +4,13 @@ import constants.Constants;
 import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 
+import Sprites.Background;
+
 public class GameLost {
     // Class variables.
     private MenuButton menuBtn;
     private MenuButton quitBtn;
+    public Background bg;
 
     public GameLost() {
         initButtons();
