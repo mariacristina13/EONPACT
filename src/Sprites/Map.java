c0b9bf51c2fcd63e7cdc2c45c5cb1df72aaadee6
haliwecc <halimatoyawoye4@gmail.com>
2026-04-23 07:35:13 +0100

    implement move log method

--- src/Sprites/Map.java
+++ src/Sprites/Map.java
@@ -1,5 +1,7 @@
 package Sprites;
 
+import constants.Constants;
+
 public class Map extends Sprite{
 private boolean move;
 private int logDirection=0;
@@ -52,11 +54,24 @@ private int logDirection=0;
 		return logDirection;
 	}
 	
+	
+	
 	public void setLogDirection(int logDirection) {
-		
+		this.logDirection = logDirection;
 	}
-    public void moveLog(int direction) {
-    	
+
+
+	public void moveLog(int direction) {
+    	if(!mobility()) {//checks if movement is not allowed
+    		return;
+    		}
+    	this.logDirection=direction;
+    	int x=getX()+direction*Constants.PLAYER_SPEED;
+    	if(x<0) {x=0;}//if the new position is past the left edge of the screen reset position
+    	if(x+getWidth()>Constants.SCREEN_WIDTH) {//checks if the log goes past the right edge
+    		x=Constants.SCREEN_WIDTH-getWidth();//moves the log back so it fits inside the right edge		
+    	}
+    	setX(x);//updates the position
     }
     /*  
     public void draw(Graphics g, int panelWidth, int panelHeight) {
