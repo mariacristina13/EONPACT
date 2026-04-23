cefbedea54a2a641081381d2adf4f4925d9c7c07
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-23 14:08:36 +0100

    Add  unimplemented test for the elephant button.

---
+++ Tests/griffith/TestUnlockingCharacters.java
@@ -0,0 +1,13 @@
+package griffith;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import game.CharacterButton;
+
+public class TestUnlockingCharacters {
+    @Test
+    public void testUnlockingElephant(){}
+}
