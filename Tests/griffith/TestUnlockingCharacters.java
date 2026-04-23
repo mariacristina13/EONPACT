fe27c97b2ea7704a14456c0f193bf80eb1a5a1d7
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-23 14:14:16 +0100

    Add test implementation for the lemur button.

--- Tests/griffith/TestUnlockingCharacters.java
+++ Tests/griffith/TestUnlockingCharacters.java
@@ -24,5 +24,17 @@ public class TestUnlockingCharacters {
     }
 
     @Test
-    public void testUnlockingLemur(){}
+    public void testUnlockingLemur(){
+        CharacterButton lemurButton = new CharacterButton("test", "test", 0, 0, 0, 0);
+
+        boolean actual = lemurButton.isLemmurUnlocked(4);
+        boolean actual1 = lemurButton.isLemmurUnlocked(6);
+        boolean actual2 = lemurButton.isLemmurUnlocked(12);
+        boolean actual3 = lemurButton.isLemmurUnlocked(-9);
+
+        assertFalse(actual);
+        assertTrue(actual1);
+        assertTrue(actual2);
+        assertFalse(actual3);
+    }
 }
