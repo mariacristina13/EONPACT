2b5096c8a7bbc556b514e9609817110c55067010
Svetlana Lazareva <lazarsveta2705@gmail.com>
2026-04-24 21:10:24 +0100

    Add class variables to Introduction

--- src/game/Introduction.java
+++ src/game/Introduction.java
@@ -5,8 +5,12 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 import Sprites.Background;
+import Sprites.Decor;
 import Sprites.Map;
 import constants.Constants;
+
 public class Introduction {
-    
+    // Class variables
+    public Background bg;
+    public Decor msg;
 }
