db36970ff894e9f39f9281951c9042995372d530
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-24 17:38:26 +0100

    Add check to see if the quit button was clicked.

--- src/game/GameWon.java
+++ src/game/GameWon.java
@@ -62,4 +62,9 @@ public class GameWon {
     public boolean menuButtonClicked(MouseEvent e) {
         return menuBtn.contains(e.getX(), e.getY());
     }
+
+    // Check if the quit button was pressed.
+    public boolean quitButtonClicked(MouseEvent e) {
+        return quitBtn.contains(e.getX(), e.getY());
+    }
 }
