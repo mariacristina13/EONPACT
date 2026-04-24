b89c6c7bcc4779c4e9d810580a1d8c626858e5fe
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-24 17:33:07 +0100

    Add class variables.

--- src/game/GameWon.java
+++ src/game/GameWon.java
@@ -1,5 +1,15 @@
 package game;
 
+import constants.Constants;
+import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+
+import Sprites.Background;
+
 public class GameWon {
-    
+    // Class variables.
+    private MenuButton menuBtn;
+    private MenuButton quitBtn;
+    public Background background;
 }
+
