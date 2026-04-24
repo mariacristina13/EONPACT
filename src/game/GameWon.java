7fd84a97cac189f68f09bc5e86dc4c807c59c0eb
Maria-Cristina Măndilă <dimamaria2005@outlook.com>
2026-04-24 17:34:12 +0100

    Add method to initialise the buttons.

--- src/game/GameWon.java
+++ src/game/GameWon.java
@@ -11,5 +11,19 @@ public class GameWon {
     private MenuButton menuBtn;
     private MenuButton quitBtn;
     public Background background;
+
+    // Initialise the buttons.
+    private void initButtons() {
+        // Set the width of the row that the buttons are going to be displayed.
+        int totalWidth = (Constants.BUTTON_WIDTH * 2) + Constants.BUTTON_SPACEING;
+        // Set the starting X position for the character display.
+        int startX = Constants.BUTTON_X - (totalWidth / 2);
+
+        menuBtn = new MenuButton("Menu", "menu button.png", "menu button hover.png", startX,
+                Constants.BUTTON_Y, Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
+        quitBtn = new MenuButton("Quit", "quit button.png", "quit button hover.png",
+                startX + Constants.BUTTON_WIDTH + Constants.BUTTON_SPACEING, Constants.BUTTON_Y, Constants.BUTTON_WIDTH,
+                Constants.BUTTON_HEIGHT);
+    }
 }
 
