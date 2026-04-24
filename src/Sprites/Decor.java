80e182b24ee47be557a78b715150f55e966ac94c
Svetlana Lazareva <lazarsveta2705@gmail.com>
2026-04-24 23:42:43 +0100

    Add comments and remove unused code

--- src/Sprites/Decor.java
+++ src/Sprites/Decor.java
@@ -2,6 +2,7 @@ package Sprites;
 
 public class Decor extends Sprite{
 
+    // class for various design elements 
     public Decor(String fileName, int x, int y, int width, int height) {
         super(fileName, x, y, width, height);
     }
