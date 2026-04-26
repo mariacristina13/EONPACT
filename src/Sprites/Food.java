36132eae1a7c8be2cb851927dce7834baed11b4e
Svetlana Lazareva <lazarsveta2705@gmail.com>
2026-04-26 22:36:40 +0100

    Add food animation variable

--- src/Sprites/Food.java
+++ src/Sprites/Food.java
@@ -9,11 +9,15 @@ public class Food extends Sprite {
     // Class variable.
     private boolean collected = false;
     private FoodType type;
+    //variables for floating animation
+    private float animationTick = 0f;
+    private float animationOffset = 0f;
 
     // Initialise the constructor in the parent class.
     public Food(String fileName, int x, int y, int width, int height, FoodType type) {
         super(fileName, x, y, width, height);
         this.type = type;
+        animationTick = (float)(Math.random() * Math.PI * 2);
     }
 
     public Food(String fileName, int x, int y, int width, int height) {
